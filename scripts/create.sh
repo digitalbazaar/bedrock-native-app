8a608e0b00d0c652593b16d063b038678cfda343
Andrew Jones <ajones@digitalbazaar.com>
2023-09-20 12:18:44 -0400

    Add initial create script.

---
+++ scripts/create.sh
@@ -0,0 +1,13 @@
+# first create the capacitor config
+bash ./scripts/set-capacitor-config.sh
+# then create the iOS project
+npx cap add ios
+# then create the android project
+npx cap add android
+# sync to make sure plugins are installed
+npx cap sync
+# for capacitor 4 we must patch the android project to work
+# with a service worker
+bash ./scripts/service-worker-android-patch.sh
+# last the app manifests
+bash ./scripts/set-native-app-manifests.sh
